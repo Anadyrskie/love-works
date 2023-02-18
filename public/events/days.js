9b19334b86212e57baaa15639d439ad18cd79e05
anadyrskie <anadyrskie@anadyr.us>
2023-02-18 00:28:22 -0700

    move day list to separate file

---
+++ public/events/days.js
@@ -0,0 +1,50 @@
+export const events = {
+    march10: {
+        title: 'March 10',
+        date: 'March 10th',
+        time: '5:30-9:00 PM',
+        description: 'Food, music, stargazing and fun on Ajo\'s Scenic Loop. Don\'t miss it!',
+        image: {
+            url: '/firesideJam.jpeg', alt: 'Love Works Poster'
+        },
+        location: {
+            name: 'Cowpie', address: 'Address 1', link: 'https://goo.gl/maps/HRjdutRv9YKLGnLS6'
+        },
+        panel: {
+            title: 'Day 1',
+            p1: 'Get ready for an unforgettable evening of stargazing and toe-tapping tunes out at Cowpie Mountain! Bring your chair, your instrument, and your appetite because Organ Pipe Cactus National Monument is providing the telescopes while The Kitchen Table Band (Ajo\'s premier Hillbilly Swing band) heats up the night with live music while you eat. Afterward, settle in for a fireside jam session like you haven\'t seen since the Year of the Big Onions!',
+            p2: 'Suggested Donation – $25',
+            p3: 'Tickets available by donation at the Ajo Copper News, Chamber of Commerce, and the Sonoran Desert Conference Center.'
+
+        }
+    },
+
+    march11: {
+        title: 'March 11', date: 'March 11th', description: 'Live music in the historic Ajo Plaza', image: {
+            url: '/loveworksPoster.jpeg', alt: 'Love Works Poster'
+        },
+        location: {
+            name: 'Ajo Plaza', address: 'Address 2', link: 'https://goo.gl/maps/m2tdgxzUfwTLbSVDA'
+        },
+        panel: {
+            title: 'Day 1',
+            p1: 'This card is still under construction.',
+            p3: "When it's done it will have a list of all the events for the day",
+
+        }
+    },
+    march12: {
+        title: 'March 12', date: 'March 12th', description: 'Explore what Ajo has to offer!', image: {
+            url: '/AMountain.png', alt: 'Love Works Poster'
+        },
+        location: {
+            name: 'Ajo', address: 'Address 3', link: 'https://goo.gl/maps/8aycuyMiEPG341HP8'
+        },
+        panel: {
+            title: 'Day 1',
+            p1: 'This card is still under construction.',
+            p3: "When it's done it will have a list of all the events for the day",
+
+        }
+    }
+}
