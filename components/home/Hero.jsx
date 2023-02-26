e8be79113c844095050186f6850e8f2a418e46d0
Anadyrskie <anadyrskie@anadyr.us>
2023-02-25 20:10:58 -0700

    change title wording

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -23,7 +23,7 @@ export function Hero() {
             <div className="relative max-w-7xl pt-5 md:pt-20 mx-auto pb-24 px-4 sm:px-6 lg:px-8">
                 <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
                     <span className="block">Love Works!</span>
-                   <span> <span className="inline-block text-indigo-400">Night </span> and <span className="inline-block text-orange-300">Day</span> in Ajo</span>
+                   <span> <span className="inline-block text-indigo-400">Night </span> and <span className="inline-block text-orange-300">Day</span><br />Ajo Desert Festival</span>
                     <span className='block mt-7 sm:mt-3 text-[100px]'>2023</span>
                 </h1>
             </div>
