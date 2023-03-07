d77f187dec193bf77669da042515d7d149bd3f74
anadyrskie <anadyrskie@anadyr.us>
2023-03-06 23:39:04 -0700

    fix favicon rel issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html style={{scrollBehavior:'smooth'}} lang="en">
       <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png" />
-              <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png" />
-                  <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png" />
-                      <link rel="manifest" href="../public/site.webmanifest" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                      <link rel="manifest" href="/site.webmanifest" />
                           <meta name="apple-mobile-web-app-title" content="Love Works Desert Festival" />
                               <meta name="application-name" content="Love Works Desert Festival" />
                                   <meta name="msapplication-TileColor" content="#da532c" />
