19fd16fccc2a8b29f11f861ec273a0ebe14e618b
anadyrskie <anadyrskie@anadyr.us>
2023-03-06 19:46:05 -0700

    convert schedule to table

--- pages/schedule.jsx
+++ pages/schedule.jsx
@@ -1,10 +1,230 @@
-import Image from "next/image";
-import schedule from "../public/loveWorksSchedule.jpg"
+
 export default function Schedule() {
     return (
-        <div className={`w-full`}>
-        <Image src={schedule} alt={'Love Works festival Schedule'} className={`w-full`} />
-        </div>
+        <div className={`lg:mx-auto m-10 prose prose-lg dark:prose-invert prose-slate`}>
+            <h1>Festival Schedule</h1>
+            <h2>Friday, March 10</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Time</th>
+                    <th>Event</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>5:00</td>
+                    <td>PARTY AT COW PIE UNTIL THE COWS COME HOME<br />CAR CONVOY LEAVES ROCALLA AVE AT ALLEY RD</td>
+                </tr>
+                </tbody>
+            </table>
 
-    )
+            <p>GET YOUR TICKETS NOW!<br />Tickets available at Copper News, Ajo Chamber of Commerce and the Saturday
+                Mercado</p>
+
+            <p>BRING YOUR OWN CHAIR!!!</p>
+
+            <p>Dinner, live music, stargazing, bonfire jam</p>
+
+            {/*SATURDAY MARCH 11 */}
+            <hr />
+            <h2>Saturday, March 11</h2>
+            <h2>AJO PLAZA</h2>
+            <table>
+               <thead>
+               <tr>
+                   <th>Time</th>
+                   <th>Event</th>
+               </tr>
+               </thead>
+                <tbody>
+                <tr>
+                    <td>9-10</td>
+                    <td>Opening Event: Dialactic Flowers Music/ Bonzai studio yoga on the plaza lawn. Free and open to
+                        the public
+                    </td>
+                </tr>
+                <tr>
+                    <td>9-2</td>
+                    <td>Organ Pipe Cactus National Monument Solarscope<br />Pima County Health Department Covid Vaccines
+                    </td>
+                </tr>
+                <tr>
+                    <td>9-5</td>
+                    <td>Arizona Raptor Center with rehabilitated raptors</td>
+                </tr>
+                <tr>
+                    <td>9-4</td>
+                    <td>El Sue&#241;o Americano exhibit/gallery</td>
+                </tr>
+                <tr>
+                    <td rowSpan="2">9-6</td>
+                    <td>Art, Artisan Vendors and are demonstrations and Peace Train rides for the kids</td>
+                </tr>
+                <tr>
+                    <td>Food Vendors<br />Nina's Kitchen (Rich and Ashley)<br />Ajo Pie<br />Jody Proscia<br />Ajo Sno<br />Bunny
+                        LaGrand</td>
+                </tr>
+                <tr>
+                    <td>12-4</td>
+                    <td>Beer/Wine Garden <strong>CASH ONLY!!!</strong><br />Benefits Ajo Volunteer Fire Department</td>
+                </tr>
+                <tr>
+                    <td rowSpan="8">10-6</td>
+                    <td>Bands and Entertainment</td>
+                </tr>
+                <tr>
+                    <td>PEACE RESTORED</td>
+                </tr>
+                <tr>
+                    <td>PRIMINITION</td>
+                </tr>
+                <tr>
+                    <td>BALLET FOLKLORICO</td>
+                </tr>
+                <tr>
+                    <td>FLY BY NIGHT</td>
+                </tr>
+                <tr>
+                    <td>ZANIEL AND T.O. NATIVES</td>
+                </tr>
+                <tr>
+                    <td>BETH AND TERRY</td>
+                </tr>
+                <tr>
+                    <td>KITCHEN TABLE BAND</td>
+                </tr>
+                <tr>
+                    <td>4:00</td>
+                    <td>TONY'S MARTINI BAND<br />FLAM CHEN STILTWALKERS</td>
+                </tr>
+                <tr>
+                    <td>5:00</td>
+                    <td>GERTY N THE T.O. BOYZ</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <h2>CURLEY COMMONS</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Time</th>
+                    <th>Event</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>4-7</td>
+                    <td>CLAY STUDIO, WOOD SHOP, WEAVING STUDIO OPEN!</td>
+                </tr>
+                <tr>
+                    <td>6:00</td>
+                    <td>OPEN MIC with BOBBY NARCHO.....YOUR CHANCE TO PERFORM!!!</td>
+                </tr>
+                <tr>
+                    <td>6:00</td>
+                    <td>LISTENING ROOM WITH VICKI TAPP and PRIMINITION - original music</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <h2>3 NATIONS</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Time</th>
+                    <th>Event</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>6:00</td>
+                    <td>DESERT GLASS<br />FLAM CHEN STILTWALKERS</td>
+                </tr>
+                <tr>
+                    <td>7:00</td>
+                    <td>ZANIEL AND T.O. NATIVES</td>
+                </tr>
+                <tr>
+                    <td>8:00</td>
+                    <td>GERTY N THE T.O. BOYZ</td>
+                </tr>
+                </tbody>
+            </table>
+    <hr />
+            <h2 className={`mb-1`}>Sunday, March 12</h2>
+            <h3>DISCOVER AJO</h3>
+            <table>
+                <thead>
+                <tr>
+                    <th>Time</th>
+                    <th>Event</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>9-10</td>
+                    <td>MIMI’S WEEKLY SUNDAY COMMUNITY BREAKFAST<br />$10/plate FEATURING MUSIC BY BETH AND TERRY<br />SONORAN
+                        DESERT INN AND CONFERENCE CENTER</td>
+                </tr>
+                <tr>
+                    <td>9-12</td>
+                    <td>FRUIT TREE PROPAGATION WORKSHOP WITH JESUS GARCIA.*$20-$60 suggested donation*<br />SONORAN DESERT
+                        INN AND CONFERENCE CENTER</td>
+                </tr>
+                <tr>
+                    <td>9-3</td>
+                    <td>HUMANITARIAN AID OFFICE OPEN (southern most free standing building beyond 3 Nations)</td>
+                </tr>
+                <tr>
+                    <td>9:30-1:30</td>
+                    <td>BIRDING WITH ORPI RANGERS AT QUITOBOQUITO</td>
+                </tr>
+                <tr>
+                    <td>10:00</td>
+                    <td>FAMILY BIKERIDE AROUND AJO. MEET AT THE FLAGPOLE AT THE PLAZA</td>
+                </tr>
+                <tr>
+                    <td>10-11</td>
+                    <td>FLAM CHEN STILTWALKERS - up close and personal performance!</td>
+                </tr>
+                <tr>
+                    <td>10-2</td>
+                    <td>MOST AJO STORES AND GALLERIES WILL BE OPEN</td>
+                </tr>
+                <tr>
+                    <td>10-2</td>
+                    <td>SONORAN DESERT WINDS STUDIO OPEN HOUSE - PAPER MAKING<br />1640 N Second Ave</td>
+                </tr>
+                <tr>
+                    <td>10-2</td>
+                    <td>THE COPPER JAVELINA</td>
+                </tr>
+                <tr>
+                    <td>10-2</td>
+                    <td>CAPTAIN KIRK'S - BUY SELL TRADE</td>
+                </tr>
+                <tr>
+                    <td>5:30a-12</td>
+                    <td>ROADRUNNER CAFE</td>
+                </tr>
+                <tr>
+                    <td>10-2</td>
+                    <td>NEWCOMER BOOTH AT PLAZA - get lots of info about your new town</td>
+                </tr>
+                <tr>
+                    <td>1-2</td>
+                    <td>AJO HISTOTY WALK WITH BILL PERRY</td>
+                </tr>
+                <tr>
+                    <td>3-4:30</td>
+                    <td>CRYSTAL BOWL HEALING SESSION AT BONZAI STUDIO</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <p><strong>SEE YOU NEXT YEAR! STAY TUNED FOR DETAILS!</strong></p>
+        </div>
+        )
 }
