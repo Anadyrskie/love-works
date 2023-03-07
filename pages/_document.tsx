81665d7434eb31e8f50d48dfd9cce35377723c24
anadyrskie <anadyrskie@anadyr.us>
2023-03-06 23:24:39 -0700

    favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html style={{scrollBehavior:'smooth'}} lang="en">
-      <Head />
+      <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="../public/apple-touch-icon.png" />
+              <link rel="icon" type="image/png" sizes="32x32" href="../public/favicon-32x32.png" />
+                  <link rel="icon" type="image/png" sizes="16x16" href="../public/favicon-16x16.png" />
+                      <link rel="manifest" href="../public/site.webmanifest" />
+                          <meta name="apple-mobile-web-app-title" content="Love Works Desert Festival" />
+                              <meta name="application-name" content="Love Works Desert Festival" />
+                                  <meta name="msapplication-TileColor" content="#da532c" />
+                                      <meta name="theme-color" content="#ffffff" />
+
+        </Head>
       <body className='dark:bg-gray-800 bg-white dark:text-white'>
         <Main />
         <NextScript />
